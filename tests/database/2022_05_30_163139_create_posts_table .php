c5144b879e9f8b5747fa52fa1db4c6d3c0241986
Ruben Robles <d8vjork@outlook.com>
2022-09-05 11:54:31 +0200

    fix test migrations

--- tests/database/2022_05_30_163139_create_posts_table .php
+++ tests/database/2022_05_30_163139_create_posts_table .php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id')->primary();
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content')->nullable();
--- tests/database/2022_05_30_163424_create_tags_table.php
+++ tests/database/2022_05_30_163424_create_tags_table.php
@@ -14,7 +14,7 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id')->primary();
             $table->string('name');
             $table->string('slug')->unique();
 
